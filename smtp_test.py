b4185548604e2e97bee09f0a88ea42230999fd2e
Brian Browning <bwbrowning@gmail.com>
2010-08-19 14:51:36 +0200

    added more message parsing examples to smtp_test.py

--- smtp_test.py
+++ smtp_test.py
@@ -1,8 +1,14 @@
 import smtpserver
 import tornado.ioloop
+import email
 
 def handle_msg(msg):
+    print "##### NEW MESSAGE ####"
+    print "from: " + msg['from']
     print "subject: " + msg['subject']
+    x = email.iterators.body_line_iterator(msg)
+    for line in x:
+        print line
 
 smtp_server = smtpserver.SMTPServer(handle_msg)
 smtp_server.listen(25)
